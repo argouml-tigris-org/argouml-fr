b3aaf8df0a0cf963aa767deb3b0b642728837dcb
ArgoUML Nightly Build monitored by Linus Tolke <closettop_nightlybuild@tigris.org>
2012-06-24 19:31:55 +0000

    Added tests.
    Submitted-by: Linus Tolke
    Change-Id: I85198c450db0b314741a90a2c2303024af27149c
    
    git-svn-id: http://argouml-fr.tigris.org/svn/argouml-fr/trunk@256 b60e1088-b31d-0410-87c8-cae6548ef484

---
+++ tests/org/argouml/i18n/TestFrKeys.java
@@ -0,0 +1,54 @@
+/* $Id$
+ *******************************************************************************
+ * Copyright (c) 2012 Contributors - see below
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Linus Tolke
+ *******************************************************************************
+ */
+
+package org.argouml.i18n;
+
+import java.util.Collection;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class TestFrKeys extends CheckKey {
+    @Parameters
+    public static Collection<Object[]> getAllKeys() {
+        return CheckKey.getKeysFor(
+                new Locale("fr"),
+                new CheckKey.IgnoredKey() {
+                    public boolean ignore(String theKey,
+                                          Locale theLocale,
+                                          ResourceBundle theLabels,
+                                          ResourceBundle theRootLabels) {
+                        return theKey.matches("parsing..*")
+                                || theKey.matches("profile..*")
+                                || theKey.matches("statusmsg..*")
+                                || theKey.matches("wfr..*")
+
+                                || theKey.matches("argouml-sql..*")
+                                || theKey.matches("cpp..*")
+                                || theKey.matches("java..*");
+                    }
+                }
+                );
+    }
+
+    public TestFrKeys(String theKey, Locale theLocale,
+                      ResourceBundle theLabels,
+                      ResourceBundle theRootLabels) {
+        super(theKey, theLocale, theLabels, theRootLabels);
+    }
+}
